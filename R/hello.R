e5519c79ebe201e57de2a4788d78426112abe08e
David J. Bosak <dbosak01@gmail.com>
2022-05-19 15:40:58 -0400

    Add license file.

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,12 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
 
+
+#' @title A sample function
+#' @encoding UTF-8
+#' @description Here is a sample function.
+#' @details
+#' Some details about the sample function.
+#' @return Nothing.
+#' @export
 hello <- function() {
   print("Hello, world!")
 }
